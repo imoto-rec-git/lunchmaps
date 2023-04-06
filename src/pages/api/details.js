a29cfd0356dc5faaa82c72e893c86c35265b7cb7
kazuya imoto <qu89ik66@gmail.com>
2023-04-07 03:11:06 +0900

    店詳細レイアウト調整

--- src/pages/api/details.js
+++ src/pages/api/details.js
@@ -1,10 +1,12 @@
 const details = async (req, res) => {
-  const { placeId } = req.query;
+  const { place_id } = req.query
   const response = await fetch(
-    `https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&place_id=${placeId}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
-  );
-  const data = await response.json();
-  res.status(200).json(data);
-};
+    `https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&place_id=${place_id}&language=ja`
+  )
+  const data = await response.json()
+  res.status(200).json(data)
+}
 
-export default details;
+export default details
+
+// https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&key=AIzaSyALKPhTFJYoWODv6U1RyCvHDKkNDl9_Z9k&place_id=ChIJN1t_tDeuEmsRUsoyG83frY4
--- src/pages/api/places.js
+++ src/pages/api/places.js
@@ -1,16 +1,12 @@
 const places = async (req, res) => {
-  const { location } = req.query;
+  const { location } = req.query
   const response = await fetch(
     `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&location=${location}&radius=150&type=restaurant&keyword="ランチ"&language=ja`
-  );
-  const data = await response.json();
-  res.status(200).json(data);
-};
+  )
+  const data = await response.json()
+  res.status(200).json(data)
+}
 
-export default places;
+export default places
 
 // https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyALKPhTFJYoWODv6U1RyCvHDKkNDl9_Z9k&location=34.69299270474642,135.49621535648794&radius=200&type=restaurant&keyword="ランチ"&language=ja
-
-// https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJCemqKvDmAGARaa9f56LdsFk&key=AIzaSyALKPhTFJYoWODv6U1RyCvHDKkNDl9_Z9k
-
-// https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyALKPhTFJYoWODv6U1RyCvHDKkNDl9_Z9k

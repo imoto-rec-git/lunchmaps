3cae4d69a0f0c8317143c2298c1aee4ba43c8398
kazuya imoto <qu89ik66@gmail.com>
2023-04-16 06:21:35 +0900

    details.js（API）の修正

--- src/components/molecules/Map/index.tsx
+++ src/components/molecules/Map/index.tsx
@@ -1,5 +1,5 @@
-import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
-import React from 'react';
+import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api'
+import React from 'react'
 
 export const Map = ({
   positionLat,
@@ -18,51 +18,49 @@ export const Map = ({
   const center = {
     lat: positionLat,
     lng: positionLng,
-  };
-  const zoom = 18;
+  }
+  const zoom = 18
   const options = {
     disableDefaultUI: true,
-  };
+  }
   const getMakerIcon = (place) => {
-    let iconPath;
+    let iconPath
     if (place.rating >= 4 && place.user_ratings_total > 80) {
-      iconPath = './images/good.svg';
+      iconPath = './images/good.svg'
     } else if (place.price_level <= 2) {
-      iconPath = './images/reasonable.svg';
+      iconPath = './images/reasonable.svg'
     } else {
-      iconPath = './images/normal.svg';
+      iconPath = './images/normal.svg'
     }
-    return iconPath;
-  };
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
+    return iconPath
+  }
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: apiKey,
-  });
+  })
   const handleRestaurantClick = (data) => {
     if (data.place_id) {
-      fetch(
-        `https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&place_id=${data.place_id}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
-      )
+      fetch(`/api/details?place_id=${data.place_id}`)
         .then((res) => res.json())
         .then((detail_data) =>
           setShopBusinessHours(detail_data.result.opening_hours.weekday_text)
         )
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
     }
-    data.name && setShopName(data.name);
-    data.opening_hours && setShopOpen(data.opening_hours.open_now);
+    data.name && setShopName(data.name)
+    data.opening_hours && setShopOpen(data.opening_hours.open_now)
     data.photos &&
       setShopPhoto(
         data.photos !== undefined &&
           `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${data.photos[0].photo_reference}&key=${apiKey}`
-      );
-    data.vicinity && setShopAddress(data.vicinity);
-    data.rating && setShopRating(data.rating);
-    data.user_ratings_total && setRatingTotal(data.user_ratings_total);
-    setPlaceId(data.place_id);
-    setActive('active');
-  };
+      )
+    data.vicinity && setShopAddress(data.vicinity)
+    data.rating && setShopRating(data.rating)
+    data.user_ratings_total && setRatingTotal(data.user_ratings_total)
+    setPlaceId(data.place_id)
+    setActive('active')
+  }
   return (
     <>
       {isLoaded && places && (
@@ -90,10 +88,10 @@ export const Map = ({
         </GoogleMap>
       )}
     </>
-  );
-};
+  )
+}
 
 const containerStyle = {
   width: '100%',
   height: 'calc(100svh - 70px)',
-};
+}
--- src/pages/api/details.js
+++ src/pages/api/details.js
@@ -1,10 +1,10 @@
 const details = async (req, res) => {
-  const { placeId } = req.query;
+  const { place_id } = req.query
   const response = await fetch(
-    `https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&place_id=${placeId}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
-  );
-  const data = await response.json();
-  res.status(200).json(data);
-};
+    `https://maps.googleapis.com/maps/api/place/details/json?fields=opening_hours&place_id=${place_id}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&language=ja`
+  )
+  const data = await response.json()
+  res.status(200).json(data)
+}
 
-export default details;
+export default details

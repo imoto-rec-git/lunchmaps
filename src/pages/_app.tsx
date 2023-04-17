db1e2a439e282dc78c0d86a80adf466effff9170
k.imoto <121611914+imoto-rec-git@users.noreply.github.com>
2023-04-17 14:16:01 +0900

    いらないセミコロン削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "@/styles/globals.css"
-import "@/styles/reset.css"
-import type { AppProps } from "next/app"
-import { IsAuthProvider } from "../providers/IsAuthProvider"
+import '@/styles/globals.css'
+import '@/styles/reset.css'
+import type { AppProps } from 'next/app'
+import { IsAuthProvider } from '../providers/IsAuthProvider'
 // import { DataProvider } from "../providers/DataProvider"
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,7 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <IsAuthProvider>
         {/* <DataProvider> */}
-        <Component {...pageProps} />;{/* </DataProvider> */}
+        <Component {...pageProps} />
+        {/* </DataProvider> */}
       </IsAuthProvider>
     </>
   )

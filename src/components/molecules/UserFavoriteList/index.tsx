1e5e5e28abaf802d09dccbc70f6913eb93521855
k.imoto <121611914+imoto-rec-git@users.noreply.github.com>
2023-05-09 15:33:27 +0900

    未使用のimport削除

--- src/components/molecules/UserFavoriteList/index.tsx
+++ src/components/molecules/UserFavoriteList/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import React from 'react'
 import { css } from '@emotion/react'
 import { useRouter } from 'next/router'
 import { useFavoriteDetail } from '@/hooks/useFavoriteDetail'

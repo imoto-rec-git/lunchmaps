223bd71366565bb7b5f59ac166293c1584d5a4d8
k.imoto <121611914+imoto-rec-git@users.noreply.github.com>
2023-05-16 16:50:02 +0900

    店舗情報（マーカー用）から60件まで取得する方法を調べる

--- src/pages/api/places.js
+++ src/pages/api/places.js
@@ -8,3 +8,33 @@ const places = async (req, res) => {
 }
 
 export default places
+
+// const places = async (req, res) => {
+//   const { location } = req.query
+
+//   let results = []
+//   let nextPageToken = null
+
+//   while (results.length < 60) {
+//     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${
+//       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+//     }&location=${location}&radius=150&type=restaurant&keyword="ランチ"&language=ja&pagetoken=${
+//       nextPageToken || ''
+//     }`
+
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     results.push(...data.results)
+
+//     if (data.next_page_token) {
+//       nextPageToken = data.next_page_token
+//     } else {
+//       break
+//     }
+//   }
+
+//   console.log(results.length)
+//   res.status(200).json({ results })
+// }
+
+// export default places
